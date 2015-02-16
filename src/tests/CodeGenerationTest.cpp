80d91740769996984d11d3545549ac8aebff0d13
Sebastien Wagener <sebastien.wagener@gmail.com>
2015-02-16 21:48:16 +0100

    code generation test: factor out function compilation

--- src/tests/CodeGenerationTest.cpp
+++ src/tests/CodeGenerationTest.cpp
@@ -9,6 +9,7 @@
 #include <llvm/Support/TargetSelect.h>
 
 #include <iostream>
+#include <functional>
 
 #include "AST.hpp"
 #include "CodeGenerator.hpp"
@@ -19,24 +20,32 @@ struct LlvmFixture {
     }
 };
 
-BOOST_GLOBAL_FIXTURE( LlvmFixture );
+BOOST_GLOBAL_FIXTURE( LlvmFixture )
 
-BOOST_AUTO_TEST_CASE(minTest) {
-    using namespace pegsolitaire::ast;
-    Variable<unsigned long> a {"a"};
-    Variable<unsigned long> b {"b"};
-    Expression<unsigned long> expr = min(a, b);
-    pegsolitaire::ast::Function<unsigned long, unsigned long, unsigned long> function {"minimum", expr, a, b};
-
+template<class Ret, class... Types>
+Ret(*compile(const pegsolitaire::ast::Function<Ret, Types...> & function))(Types...) {
     using namespace llvm;
-
-    Module * module = new Module("minTest", getGlobalContext());
+    // uint64_t(*)(u_int64_t, uint64_t)
+    Module * module = new Module("compiledModule", getGlobalContext());
     IRBuilder<> builder(getGlobalContext());
     pegsolitaire::codegen::CodeGenerator cg(module, builder);
     auto f = cg(function);
     //module->dump();
     ExecutionEngine * executionEngine = EngineBuilder(module).create();
-    auto fp = reinterpret_cast<uint64_t(*)(u_int64_t, uint64_t)>(executionEngine->getPointerToFunction(f));
+    // TODO we actually need a mapping from Types... to NativeTypes... here:
+    return reinterpret_cast<Ret(*)(Types...)>(executionEngine->getPointerToFunction(f));
+}
+
+BOOST_AUTO_TEST_CASE(minTest) {
+
+    using namespace pegsolitaire::ast;
+
+    Variable<unsigned long> a {"a"};
+    Variable<unsigned long> b {"b"};
+    Function<unsigned long, unsigned long, unsigned long> function = {"minimum", min(a, b), a, b};
+
+    auto fp = compile(function);
+
     BOOST_CHECK_EQUAL(fp(4,7), 4);
     BOOST_CHECK_EQUAL(fp(10,7), 7);
 }
